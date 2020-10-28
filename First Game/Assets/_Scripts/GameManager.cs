aa9a86e0ed3efff715988af427a9843056225733
SamTheTotallyNormalPerson <40128566+SamTheTotallyNormalPerson@users.noreply.github.com>
2020-10-28 07:20:42 -0400

    qawfsdf

---
+++ First Game/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+public class GameManager : MonoBehaviour
+{
+
+    public int currentGold;
+    public TMP_Text goldText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddGold(int goldtoAdd)
+    {
+        currentGold += goldtoAdd;
+        goldText.text = "Gold: " + currentGold + "!";
+    }
+}
---
+++ First Game/Assets/_Scripts/WumpaPickUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class WumpaPickUp : MonoBehaviour {
+
+    public int value;
+    //public AudioSource wumpapickyupper;
+    public float destoryTime;
+    public GameObject wumpaMesh;
+    public float lookRadius = 10f;
+    public GameObject pickupEffect;
+
+   public Transform target;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+
+        
+       
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //wumpapickyupper.Play();
+            FindObjectOfType<GameManager>().AddGold(value);
+            Destroy(gameObject);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            
+        }
+    }
+
+   void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+}
